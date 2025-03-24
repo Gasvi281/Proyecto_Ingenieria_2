0dd024180f58d19ea2842da0a4b90f83ee19bd3c
Gasvi281 <egaviria011@gmail.com>
2025-03-24 14:41:34 -0500

    Inicio de implementacion de relaciones

--- controllers/CuentaController.js
+++ controllers/CuentaController.js
@@ -1,65 +1,77 @@
 const { Cuenta } = require("../models");
+const CuentaImpedimientos = require("../models/CuentaImpedimientos");
+const CuentaPreferencias = require("../models/CuentaPreferencias");
 
 const getCuentaById = async (req, res) => {
-        const {id}= req.params;
+    const { id } = req.params;
 
-        const cuenta = await Cuenta.findOne({where: {id}});
+    const cuenta = await Cuenta.findOne({
+        where: { id }, include: [{
+            model: CuentaImpedimientos, as: "CuentaImpedimientos",
+            include: [{ model: Producto, as: "Producto" }]
+        }],
+        include: [{
+            model: CuentaPreferencias, as: "CuentaPreferencias",
+            include: [{ model: Producto, as: "Producto" }]
+        }]
+    });
 
-        if(!cuenta){
-        res.status(404).json({error: "uPS"});
-        }
+    if (!cuenta) {
+        res.status(404).json({ error: "uPS" });
+    }
 
-        res.status(200).json(cuenta);
-    
+    res.status(200).json(cuenta);
+
 }
 
-const addCuenta=async(req, res)=>{
+const addCuenta = async (req, res) => {
     try {
-        const {nombreUsuario, 
-            email, 
-            nombre, 
+        const { nombreUsuario,
+            email,
+            nombre,
             // fotoPerfil
-        }= req.body;
+        } = req.body;
 
-        const cuenta= await Cuenta.create({nombreUsuario, 
-            email, 
-            nombre, 
+        const cuenta = await Cuenta.create({
+            nombreUsuario,
+            email,
+            nombre,
             // fotoPerfil
         })
         res.status(201).json(cuenta);
 
 
-    }catch(error){
-        res.status(500).json({error: error})
+    } catch (error) {
+        res.status(500).json({ error: error })
     }
 }
 
 
-const updateCuenta=async(req, res)=>{
+const updateCuenta = async (req, res) => {
     try {
-        const{id}=req.params;
-        const{
-            nombreUsuario, 
-            email, 
-            nombre, 
+        const { id } = req.params;
+        const {
+            nombreUsuario,
+            email,
+            nombre,
             // fotoPerfil
-        }= req.body;
-        const cuenta=await Usuario.findByPk(id);
+        } = req.body;
+        const cuenta = await Usuario.findByPk(id);
 
-        if(!cuenta){
-            return res.status(404).json({message: "usuario no encontrado"});
+        if (!cuenta) {
+            return res.status(404).json({ message: "usuario no encontrado" });
         }
-        if(nombreUsuario) cuenta.nombreUsuario=nombreUsuario;
-        if(nombre) cuenta.nombre=nombre;
-        if(email) cuenta.email=email;
+        if (nombreUsuario) cuenta.nombreUsuario = nombreUsuario;
+        if (nombre) cuenta.nombre = nombre;
+        if (email) cuenta.email = email;
         // if (fotoPerfil) cuenta.fotoPerfil=fotoPerfil;
 
         await cuenta.save();
-        return res.status(200).json({message: "Usuario actualizado", usuario});
+        return res.status(200).json({ message: "Usuario actualizado", usuario });
 
 
-    }catch(error){
-        res.status(500).json({error: error.message})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 }
 
